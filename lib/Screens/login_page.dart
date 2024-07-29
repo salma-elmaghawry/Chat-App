67e051da9032c7d19d11ce7aff8d81f7d0248add
salma <salmaelmaghawry91@gmail.com>
2024-07-29 05:56:26 +0300

    email ans password done

--- lib/Screens/login_page.dart
+++ lib/Screens/login_page.dart
@@ -6,53 +6,76 @@ class loginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Column(
-        children: [
-          SizedBox(height: 80),
-          Image.asset(
-            'assets/chat.png',
-            height: 200,
+    return  Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0), // Adjust padding as needed
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Spacer(flex: 3),
+              Center(
+                child: Image.asset(
+                  'assets/chat.png',
+                  height: 200,
+                ),
+              ),
+              Spacer(flex: 1),
+              Center(
+                child: Text(
+                  'LOGIN',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xff36454F),
+                  ),
+                ),
+              ),
+              Spacer(flex: 2),
+              Padding(
+                padding: const EdgeInsets.only(left: 7.0, bottom: 5.0),
+                child: Text('Email', style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500, color: Color(0xffFF7F50))),
+              ),
+              customTextField(
+                hint: 'johndoe@example.com',
+                suffixIcon: Icon(Icons.email),
+              ),
+              Spacer(flex: 1),
+              Padding(
+                padding: const EdgeInsets.only(left: 7.0, bottom: 5.0),
+                child: Text('Password', style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500, color: Color(0xffFF7F50))),
+              ),
+              //pacer(flex: 1),
+              customTextField(
+                hint: '*******',
+                suffixIcon: Icon(Icons.password_sharp),
+              ),
+              Spacer(flex: 2),
+              SizedBox(
+                width: double.infinity, 
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xffFF7F50),
+                    minimumSize: Size(double.infinity, 60), 
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50), 
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'Log in',
+                    style: TextStyle(
+                      fontSize: 19,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Spacer(flex: 10),
+            ],
           ),
-          SizedBox(height: 20),
-          Text('LOGIN',
-              style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.w500,
-                  color: Color(0xff36454F))),
-                  SizedBox(height: 20),
-
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left:20.0),
-                        child: Text('Email',style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500,color: Color(0xffFF7F50)),),
-                      ),
-                    ],
-                    
-                  ),
-          SizedBox(height: 10),
-          customTextField(),
-          SizedBox(height: 20),
-          Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left:20.0),
-                        child: Text('Password',style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500,color: Color(0xffFF7F50)),),
-                      ),
-                    ],
-                    
-                  ),
-          SizedBox(height: 10),
-          customTextField(),
-          
-          
-          
-    
-
-        ],
+        ),
       ),
-    ));
+    );
   }
 }
--- lib/widgets/customTextField.dart
+++ lib/widgets/customTextField.dart
@@ -1,34 +1,46 @@
 import 'package:flutter/material.dart';
 
 class customTextField extends StatelessWidget {
-  const customTextField({super.key});
+  customTextField({
+    super.key,
+    required this.hint,
+    this.suffixIcon,
+  });
+  String? hint;
+  Icon? suffixIcon;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-      
-      cursorColor: Color(0xffFF7F50),
-    
+       cursorColor: Color(0xffFF7F50),
       decoration: InputDecoration(
-        
-          filled: true,
-          fillColor: Color(0xFFD3D3D3),
-          focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(50),
-              borderSide: BorderSide(width: 2.0, color: Color(0xffFF7F50))),
-          enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(50),
-              borderSide: BorderSide(
-                width: 0.0,
-                color: Colors.transparent,
-              )),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(50),
-            borderSide: BorderSide(
-              width: 0.0,
-              color: Colors.transparent,
-            ),
-          )),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        suffixIcon: suffixIcon,
+        suffixIconColor: Color(0xffFF7F50),
+        hintText: hint,
+        hintStyle: TextStyle(fontSize: 15, color: Color.fromARGB(255, 156, 155, 155)),
+        filled: true,
+        fillColor: Color(0xFFD3D3D3),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50),
+          borderSide: BorderSide(width: 2.0, color: Color(0xffFF7F50)),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50),
+          borderSide: BorderSide(
+            width: 0.0,
+            color: Colors.transparent,
+          ),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50),
+          borderSide: BorderSide(
+            width: 0.0,
+            color: Colors.transparent,
+          ),
+        ),
+      ),
     );
+    
   }
 }
